ed09634cd68b1a71f548954507c7b5a64b02724d
Dixie <nope>
2023-04-30 23:22:56 +0300

    Updating to-do-list.

--- todolist.js
+++ todolist.js
@@ -1,13 +1,14 @@
 /*
 
- automate building (1/3 of 3/3 - automated SPAWNroad building completed)
- automate expansion
+ automate building (1/3 - automated SPAWNroad building completed)
+ automate expansion (1/3 - compleated, scouts or claimers are now will claim rooms automaticly, WIP room-message, WIP room-reserving)
  
- automate defend
+ automate defend (1/3 - all towers now shooting enemies, WIP different target for each turret, WIP automat ACTIVE defense or REFACTORING )
  
- automate fixing
+ automate fixing (1/1 - maybe diffirent targets)
+ 
  automating creep spawn (completed with kostil, waiting for its turn to be refactored)
   create cool find resorces code and insert to all resource needed creeps (create a new file with function to find resource in room, if found giving
-                                                                          route and creep go to it and withdraw/gather it) COMPLETED
+                                                                          route and creep go to it and withdraw/gather it) COMPLETED 2/3 NOW NEED WITH PICKERS
  */
 
