d12884cb0dc4db23adb11dbc75b5956d5e74daa2
ItsDixie <hipster.yt89@mail.ru>
2023-05-03 20:09:59 +0300

    attacker tweaks

--- role.attacker.js
+++ role.attacker.js
@@ -2,7 +2,7 @@ var attacker = {
     run: function(creep){
         var targets = creep.room.find(FIND_HOSTILE_CREEPS);
         if(targets.length > 0){
-            if(creep.attack(targets[0])){ // <-- added closing parenthesis
+            if(creep.attack(targets[0])){ 
                 creep.moveTo(targets[0])
                 creep.say("Intruder!", {public: true})
             }
