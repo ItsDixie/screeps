c401a21a8e581192c555cfc26778fe1aa1bdc3c8
ItsDixie <hipster.yt89@mail.ru>
2023-05-05 22:29:55 +0300

    Small update
    Info + tweaks

--- role.wallkeeper.js
+++ role.wallkeeper.js
@@ -7,16 +7,16 @@ var wallKeeper = {
         var walls = creep.room.find(FIND_STRUCTURES, {filter:{structureType: STRUCTURE_WALL}})
         var ramparts = creep.room.find(FIND_STRUCTURES, {filter:{structureType: STRUCTURE_RAMPART}})
         
-	    if(creep.memory.repairing && creep.store[RESOURCE_ENERGY] == 0) {
+	    if(creep.memory.repairing && creep.store[RESOURCE_ENERGY] == 0) {   /* <- look here for optimise road using */
             creep.memory.repairing = false;
 	    }
-	    if(!creep.memory.repairing && creep.store.getFreeCapacity() == 0) {
+	    if(!creep.memory.repairing && creep.store.getFreeCapacity() == 0) { /* <- look here for optimise road using */
 	        creep.memory.repairing = true;
 	    }
 	    
 	    if(creep.memory.repairing){
 	        for(var wall in walls){
-	            if(walls[wall].hits < 1000){
+	            if(walls[wall].hits < 30000){     /* <- look here for handle big hp walls */
                     try{
                         if(creep.moveByPath(Room.deserializePath(creep.memory.path)) == -5){
                             utils.path(creep, walls[wall])
@@ -34,7 +34,7 @@ var wallKeeper = {
 	            
 	            }
 	        }
-	        for(var rampart in ramparts){
+	        for(var rampart in ramparts){                /* <- ramparts don't fixing, probably bad priority and decaying */
 	            if(ramparts[rampart].hits < 1000){
                     try{
                         if(creep.moveByPath(Room.deserializePath(creep.memory.path)) == -5){
@@ -53,10 +53,7 @@ var wallKeeper = {
 	            }
 	        }
 	    }else{
-            getResource.find1(creep, RESOURCE_ENERGY);
-            if(creep.store.getFreeCapacity() == 0){
-                creep.moveTo(creep.room.controller)
-            }
+            getResource.find1(creep, RESOURCE_ENERGY);    /* <- look here for optimise road using */
 	    }
         
     }
